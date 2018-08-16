7f470e79111977e19df1dcd17d932189c1f94f6b
ASUS-A442U <ASUS-A442U@LAPTOP-0B7VJDRL.mshome.net>
2018-08-16 18:32:30 +0700

    Initial Commit

---
+++ src/convertvocal2angka/ConvertVokal2Angka.java
@@ -0,0 +1,22 @@
+
+package convertvocal2angka;
+
+import java.util.Scanner;
+
+public class ConvertVokal2Angka {
+     private static void tampilJudul(String identitas){
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+     
+     
+     public static void main(String[] args){
+        String identitas = "Alya Selvia Tamzila / X RL 2 / 03";
+        
+        tampilJudul(identitas); 
+        
+        
+        
+    }
+}
